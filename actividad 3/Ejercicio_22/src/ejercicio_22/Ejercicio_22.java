ddda69d03b33f4082312fff3e46ac6a1d999a4f7
Jhanuar12 <142674863+Jhanuar12@users.noreply.github.com>
2023-09-18 16:50:07 -0500

    Add files via upload

---
+++ actividad 3/Ejercicio_22/src/ejercicio_22/Ejercicio_22.java
@@ -0,0 +1,12 @@
+package ejercicio_22;
+public class Ejercicio_22 {
+
+
+    public static void main(String[] args) {
+    Interfaz form = new Interfaz();
+        
+    form.setVisible(true);
+
+    }
+    
+}
---
+++ actividad 3/Ejercicio_23/src/ejercicio_23/Ejercicio_23.java
@@ -0,0 +1,11 @@
+package ejercicio_23;
+
+public class Ejercicio_23 {
+
+    public static void main(String[] args) {
+    
+     Formulario form = new Formulario();
+     form.setVisible(true);
+    }
+    
+}
